1db39185bd8ef6c6b7739abe186051556c05420b
gayaneyazeryan <yazeryangayane@gmail.com>
2023-12-12 21:53:45 +0400

    initial commit

---
+++ model_svm.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
