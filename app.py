88f216f685da1c7447af58b78bbd5de439603e65
gayaneyazeryan <yazeryangayane@gmail.com>
2023-12-12 23:29:43 +0400

    some changes

--- app.py
+++ app.py
@@ -5,10 +5,7 @@ import streamlit as st
 st.set_page_config(page_title='Diabetic Prediction Web App', page_icon=':moneybag', layout='centered')
 
 
-
-loaded_model = pickle.load(open('/home/gayane/Desktop/Diabet-Detection/trained_model1.sav', 'rb'))
-
-
+loaded_model = pickle.load(open('./trained_model1.sav', 'rb'))
 
 
 def diabetes_prediction(input_data):
--- predicted_system.py
+++ predicted_system.py
@@ -4,7 +4,7 @@ import pickle
 
 st.set_page_config(page_title='Diabetic Prediction Web App', page_icon=':moneybag', layout='centered')
 
-loaded_model = pickle.load(open('/home/gayane/Desktop/MLE/trained_model1.sav', 'rb'))
+loaded_model = pickle.load(open('./trained_model1.sav', 'rb'))
 #creating a function for prediction
 
 input_data = (1, 10.0, 0, 0, 0, 22.68, 6.6,100)
