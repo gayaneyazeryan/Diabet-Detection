f8702ce0aa99ece60d0f7723c4e46ecdbc4e1419
gayaneyazeryan <yazeryangayane@gmail.com>
2023-12-12 22:33:24 +0400

    adds predicted system

---
+++ predicted_system.py
@@ -0,0 +1,21 @@
+import numpy as np
+import streamlit as st
+import pickle 
+
+st.set_page_config(page_title='Diabetic Prediction Web App', page_icon=':moneybag', layout='centered')
+
+loaded_model = pickle.load(open('/home/gayane/Desktop/MLE/trained_model1.sav', 'rb'))
+#creating a function for prediction
+
+input_data = (1, 10.0, 0, 0, 0, 22.68, 6.6,100)
+input_data_numpy = np.asarray(input_data).reshape(1,-1)
+
+# Make predictions
+predictions = loaded_model.predict(input_data_numpy)
+
+if predictions == 0:
+  print("The person is not diabetic")
+
+else:
+  print("The person is diabetic")
+
